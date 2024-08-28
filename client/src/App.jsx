5ae78853b8ac0ecd141ab5f9782d50667cdb6435
sreyascheviri <sreyascheviri@gmail.com>
2024-08-28 17:55:05 +0530

    create pages and toutes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 export default function App() {
-  return <h1 className="text-3xl text-red-900">reso</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
