3154b0e5ca0ea331dbfe19857103974723052ea0
sreyascheviri <sreyascheviri@gmail.com>
2024-09-08 05:06:54 +0530

    user models

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mogoose';
+
+const userSchema  = new mongoose.Schema({
+    username: {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type:String,
+        required: true,
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    },{timestamps: true}
+
+);
+
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
