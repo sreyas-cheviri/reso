97eca40e5984151c58ea2c591410889a0526dc8c
sreyascheviri <sreyascheviri@gmail.com>
2024-10-14 01:49:00 +0530

    remve link

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,28 +4,26 @@ import { Label ,TextInput,Button } from "flowbite-react";
 
 export default function SignUp() {
   return (
-    <div className="min-h-screen mt-20 ">
+    <div className="min-h-screen mt-10 ">
       <div className="flex p-20 max-w-3xl mx-auto flex-col md:flex-row md:items-center md:p-10 md:gap-20">
         <div  className="flex-1">
-          <Link to="/" className=" font-bold bg-clip-padding">
-            <span className="  text-white px-4 py-2 bg-black rounded-lg text-4xl ">
+            <span className="font-bold bg-clip-padding  text-white px-4 py-2 bg-black rounded-lg text-4xl ">
               100x Blogs
             </span>
-          </Link>
           <p className="text-base font-semibold mt-5">
             Join 100xdev! <br />
-            Connect, learn, and grow with fellow developers. Sign up now to
+            Learn and grow with fellow developers. Sign up now to
             unlock exclusive resources and boost your skills!
             <br />
             Let’s build together!
           </p>
         </div>
 
-        <div className="mt-10 flex-1">
+        <div className="mt-10 flex-1 border p-4 rounded-md">
           <form className="flex flex-col gap-3">
             <div>
               <Label value='Enter your Username' />
-              <TextInput  className="focus:outline-none focus:ring-4 focus:ring-gray-300" type='text' placeholder='Username' id='username'/>
+              <TextInput  className="gray-800" type='text' placeholder='Username' id='username'/>
             </div>
             <div>
               <Label value='Enter your Email' />
@@ -39,7 +37,7 @@ export default function SignUp() {
           </form>
           <div className="flex gap-2 mt-5 text-base">
             <span>Already registered?</span>
-              <Link to='/sign-in' className='text-red-500 underline '> Sign In</Link>
+              <Link to='/sign-in' className='text-red-500 underline font-bold'> Sign In</Link>
           </div>
         </div>
       </div>
